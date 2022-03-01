05f9983fb21bf80b6c35784da6e04ad6df8d3f4a
aroselee <acroselee@gmail.com>
2022-03-01 13:08:59 -0800

    Change console controller to ReportController, messaging

--- src/console/controllers/ReportController.php
+++ src/console/controllers/ReportController.php
@@ -1,33 +1,49 @@
 <?php
 
-namespace console\controllers;
+namespace trendyminds\reporter\console\controllers;
 
 Use Craft;
 
 use craft\console\Controller;
-use jobs\ExportJob;
+use trendyminds\reporter\jobs\ExportJob;
+use trendyminds\reporter\Reporter;
+use yii\helpers\Console;
 
 class ReportController extends Controller
 {
   /**
   * @var string|null The type of sync to run
   */
-  public $type = null;
+  public $handle = null;
 
   public function options($actionID)
   {
       $options = parent::options($actionID);
-      $options[] = 'type';
+      $options[] = 'handle';
 
       return $options;
   }
 
+  /**
+  * php craft reporter/report --handle=pages
+  */
   public function actionIndex()
   {
-      Craft::$app->getQueue()->push(
-          new ExportJob([
-              "type" => $this->type
-          ])
-      );
+    $report = Reporter::getInstance()->getReport($this->handle);
+
+    if(!$report){
+      $this->stderr("You must specify the handle of an existing report to export. Consult config/reporter.php to ensure you have created one and you are using the handle." . PHP_EOL, Console::FG_RED);
+      die();
+    }
+
+    $report = $report();
+    $this->stdout("Running report " . $report['name'] . "." . PHP_EOL, Console::FG_BLUE);
+
+    Craft::$app->getQueue()->push(
+			new ExportJob([
+				'handle' => $this->handle,
+				'name' => $report['name'],
+			])
+		);
   }
 }
