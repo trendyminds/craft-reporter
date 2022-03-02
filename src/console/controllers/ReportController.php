da6211714558a39aebdebc6cb17ac3aa665aab17
Aaron Bushnell <aaron.bushnell@gmail.com>
2022-03-01 19:41:42 -0500

    Convert to tabs

--- src/console/controllers/ReportController.php
+++ src/console/controllers/ReportController.php
@@ -3,7 +3,6 @@
 namespace trendyminds\reporter\console\controllers;
 
 Use Craft;
-
 use craft\console\Controller;
 use trendyminds\reporter\jobs\ExportJob;
 use trendyminds\reporter\Reporter;
@@ -11,39 +10,39 @@ use yii\helpers\Console;
 
 class ReportController extends Controller
 {
-  /**
-  * @var string|null The type of sync to run
-  */
-  public $handle = null;
+	/**
+	 * @var string|null The type of sync to run
+	 */
+	public $handle = null;
 
-  public function options($actionID)
-  {
-      $options = parent::options($actionID);
-      $options[] = 'handle';
+	public function options($actionID)
+	{
+		$options = parent::options($actionID);
+		$options[] = 'handle';
 
-      return $options;
-  }
+		return $options;
+	}
 
-  /**
-  *  Run Reporter from the command line. Must pass in --handle=<report Craft handle>
-  */
-  public function actionIndex()
-  {
-    $report = Reporter::getInstance()->getReport($this->handle);
+	/**
+	 *  Run Reporter from the command line. Must pass in --handle=<report Craft handle>
+	 */
+	public function actionIndex()
+	{
+		$report = Reporter::getInstance()->getReport($this->handle);
 
-    if(!$report){
-      $this->stderr("You must specify the handle of an existing report to export. Consult config/reporter.php to ensure you have created one and you are using the handle." . PHP_EOL, Console::FG_RED);
-      die();
-    }
+		if(!$report){
+			$this->stderr("You must specify the handle of an existing report to export. Consult config/reporter.php to ensure you have created one and you are using the handle." . PHP_EOL, Console::FG_RED);
+			die();
+		}
 
-    $report = $report();
-    $this->stdout("Running report " . $report['name'] . "." . PHP_EOL, Console::FG_BLUE);
+		$report = $report();
+		$this->stdout("Running report " . $report['name'] . "." . PHP_EOL, Console::FG_BLUE);
 
-    Craft::$app->getQueue()->push(
+		Craft::$app->getQueue()->push(
 			new ExportJob([
 				'handle' => $this->handle,
 				'name' => $report['name'],
 			])
 		);
-  }
+	}
 }
